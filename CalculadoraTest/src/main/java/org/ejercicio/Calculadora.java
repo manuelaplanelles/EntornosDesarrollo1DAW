eaac9b6caa210de7bd100fbdd5eaae6dda530821
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-13 19:17:17 +0100

    organizacion de directorios y empezado el testeo de calculadora

---
+++ CalculadoraTest/src/main/java/org/ejercicio/Calculadora.java
@@ -0,0 +1,36 @@
+package org.ejercicio;
+
+public class Calculadora {
+
+    private int primerNumero;
+    private int segundoNumero;
+
+    public Calculadora(int primerNumero, int segundoNumero) {
+        this.primerNumero = primerNumero;
+        this.segundoNumero = segundoNumero;
+    }
+
+    public int suma() {
+        int resultado = primerNumero + segundoNumero;
+        return resultado;
+    }
+
+    public int resta() {
+        // Error introducido de forma intencionada para detectarlo con JUnit
+        int resultado = primerNumero + segundoNumero;
+        return resultado;
+    }
+
+    public int multiplica() {
+        int resultado = primerNumero * segundoNumero;
+        return resultado;
+    }
+
+    public int divide() {
+        // Error introducido de forma intencionada para detectarlo con JUnit
+        int resultado = primerNumero + segundoNumero;
+        return resultado;
+    }
+}
+
+
